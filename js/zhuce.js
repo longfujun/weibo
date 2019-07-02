971abd89dde36defbbb30dd75b708469e0b2126d
songhuibing <549762993@qq.com>
2019-07-02 17:08:30 +0800

    还原

--- js/zhuce.js
+++ js/zhuce.js
@@ -21,3 +21,23 @@ $('.btn').bind('click',function(){
     }
 })
 
+$('.header').click(function(e){
+    var targe=e.target;
+    if(targe.className=='gr'){
+        $('.left-form').css('display','block')
+        $('.left-form1').css('display','none')
+        $('em').css('border-color','#fff')
+        $(targe).css('border-color','#ffa00a',)
+    }
+    if(targe.className=='gf'){
+        $('em').css('border-color','#fff')
+        $('.left-form').css('display','none')
+        $('.left-form1').css('display','block')
+        $(targe).css('border-color','#ffa00a',)
+    }
+})
+
+$('.yzm u').click(function(){
+    var cont=(getYZM(4))
+    $('.yzm span').html(cont)
+})
