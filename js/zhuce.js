77546533f8ad6bf8114d575ce5ce30554b813e97
songhuibing <549762993@qq.com>
2019-07-02 16:15:16 +0800

    no

--- js/zhuce.js
+++ js/zhuce.js
@@ -2,13 +2,15 @@ $('.btn').bind('click',function(){
     if(passwdflag && userflag){
     var userVal=$('input[name=user]').val();
     var passwdVal=$('input[name=passwd]').val();
+    
         $.ajax({
-            
             url:'../data/login2.php',
             type:'get',
-            dataType:'jsonp',
+            dataType:'json',
             data:'act=add&user='+userVal+'&pass='+passwdVal,
+            
             success:function(josn){
+                
                 console.log(josn)
                 alert(josn.msg);
             },
